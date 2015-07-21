33ffb4c6e245cf6e68657aebff8cda66b8dc1808
thejdavid <jdlavaud@gmail.com>
2015-07-21 12:53:19 +0200

    refactor

--- app/models/Processing.rb
+++ app/models/Processing.rb
@@ -1,21 +1,20 @@
 require 'blockchain'
 require 'digest/md5'
 class Processing
-  def initialize(document_value)
+  attr_reader :wallet
+  def initialize(document_path)
+    @document = document_path
     @wallet = Blockchain::Wallet.new('ada4e4b6-3c9f-11e4-baad-164230d1df67', 'password123')
-    @document = documment_value
-    @md5_hash = ""
-    @response_object = transaction.tx_hash ||= ""
-  end
-  def create
-    self.hashing_document
-    self.transaction_hash
+    @transaction = ""
   end
   #@wallet.send(adress of receipient,amount,adress sender,fee,note)
-  def transaction_hash
-    @transaction = @wallet.send('1NAF7GbdyRg3miHNrw2bGxrd63tfMEmJob', 0.0001, from_address: '1A8JiWcwvpY7tAopUkSnGuEYHmzGYfZPiq',note:@md5_hash)
+  def transaction
+    @transaction = wallet.send('1NAF7GbdyRg3miHNrw2bGxrd63tfMEmJob', 0.0001, from_address: '1A8JiWcwvpY7tAopUkSnGuEYHmzGYfZPiq',note:self.hash_document)
   end
-  def hashing_document
-    Digest::MD5.hexdigest(@document)
+  def transaction_hash
+    @transaction.tx_hash
+  end
+  def hash_document
+    Digest::MD5.hexdigest(File.read(@document))
   end
 end
