ab3b986a773f9b698e6659c6b9b9bfc3425bd628
thejdavid <jdlavaud@gmail.com>
2015-07-21 12:54:29 +0200

    working controller

--- app/controllers/certificat_controller.rb
+++ app/controllers/certificat_controller.rb
@@ -1,8 +1,14 @@
 class CertificatController < ApplicationController
   def index
+    @certificats = Certificat.find_by(user_id:current_user.id)
   end
   def create
-  end
-  def new
+    processing = Processing.new(params[:file].path)
+    certificat = Certificat.new(file_name:params[:file].original_filename, btc_transaction: processing.transaction,file_hash:processing.hash_document ,user_id:current_user)
+    if certificat.save
+      redirect_to(certificat_index_path)
+    else
+      #flash msg error
+    end
   end
 end
