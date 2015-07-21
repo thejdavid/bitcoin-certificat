d6d6675f6ccd854f67c4fc653ec18ceff54073f6
thejdavid <jdlavaud@gmail.com>
2015-07-21 13:33:02 +0200

    mvp

--- app/controllers/certificat_controller.rb
+++ app/controllers/certificat_controller.rb
@@ -1,14 +1,22 @@
 class CertificatController < ApplicationController
   def index
-    @certificats = Certificat.find_by(user_id:current_user.id)
+    @certificats = Certificat.where(user_id:current_user.id)
   end
   def create
+    #creating the certificat and saving the record
     processing = Processing.new(params[:file].path)
-    certificat = Certificat.new(file_name:params[:file].original_filename, btc_transaction: processing.transaction,file_hash:processing.hash_document ,user_id:current_user)
+    processing.transaction
+    certificat = Certificat.new(file_name:params[:file].original_filename, btc_transaction: processing.tx_hash,file_hash:processing.hash_document ,user_id:current_user)
     if certificat.save
       redirect_to(certificat_index_path)
     else
-      #flash msg error
+      flash[:notice] = "Error"
     end
+    #Saving the file
+    name = params[:file].original_filename
+    directory = "/public"
+    path = File.join(directory, name)
+    File.open(path, "wb") { |f| f.write(params[:upload][:file].read) }
+    flash[:notice] = "File uploaded"
   end
 end
