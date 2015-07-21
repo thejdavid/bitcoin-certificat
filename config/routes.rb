f5af17418a4ffd144d14abde5f0f95260dcbee13
thejdavid <jdlavaud@gmail.com>
2015-07-21 12:54:03 +0200

    removing new route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
-  resources :certificat, only: [:index, :new, :create]
+  resources :certificat, only: [:index, :create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
